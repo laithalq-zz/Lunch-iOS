3a8e4a5628c097b21442b267e2224e99626aa00e
laithalq <laithalq@gmail.com>
2014-10-04 01:57:57 -0700

    Initial Commit

---
+++ Lunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lunch
+//
+//  Created by Laith Alqaisi on 10/4/14.
+//  Copyright (c) 2014 CalHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lunch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lunch
+//
+//  Created by Laith Alqaisi on 10/4/14.
+//  Copyright (c) 2014 CalHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
